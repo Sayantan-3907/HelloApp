713642ba8ab49c72a9372ddd2a3667c2e6a7d87e
Sayantan Biswas <lordbiswas333@gmail.com>
2026-04-10 16:47:18 +0530

    Coded UC7: Display Hello using String.join() method

---
+++ HelloAppUC7.java
@@ -0,0 +1,13 @@
+@author Sayantan
+@version 1.0
+
+public class HelloApp {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            System.out.println("Hello, " + String.join(", ", args) + "!");
+        }
+    }
+}
